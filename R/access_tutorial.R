34d319468644f2fb8770feee1283ec79ccd7ab3c
Isaac Quintanilla <inqs.909@gmail.com>
2021-04-16 20:09:14 -0700

    Added R documentations and made minor changes to description file to pass "R CMD check".

--- R/access_tutorial.R
+++ R/access_tutorial.R
@@ -1,3 +1,12 @@
+#' access_tutorial
+#' 
+#' A wrapper function to deploy a learnr tutorial in the ucrstats package.
+#'
+#' @param tutorial Name of tutorial you wish to access.
+#'
+#' @export
+#'
+#' 
 access_tutorial<-function(tutorial){
   learnr::run_tutorial(name=tutorial,package = "ucrstats")
 }
---
+++ R/data.R
@@ -0,0 +1,40 @@
+#' A simulated dataset used to represent long data.
+#'
+#'
+#' @format A data frame with 4000 rows and 5 variables:
+#' \describe{
+#'   \item{ID1}{An ID variable grouping the repeated measurements}
+#'   \item{time}{The time point for each repeated measurement}
+#'   \item{mean}{mean of observation at time point}
+#'   \item{sd}{mean of observation at time point}
+#'   \item{median}{mean of observation at time point}
+#'  }
+#'  
+#'
+"long"
+
+#' A simulated dataset used to represent wide data.
+#'
+#'
+#' @format A data frame with 1000 rows and 13 variables:
+#' \describe{
+#'   \item{ID1}{An ID variable grouping the repeated measurements}
+#'   \item{v1/mean}{Variable representing the mean at time point 1}
+#'   \item{v1/median}{Variable representing the median at time point 1}
+#'   \item{v1/sd}{Variable representing the sd at time point 1}
+#'   \item{v2/mean}{Variable representing the mean at time point 2}
+#'   \item{v2/median}{Variable representing the median at time point 2}
+#'   \item{v2/sd}{Variable representing the sd at time point 2}
+#'   \item{v3/mean}{Variable representing the mean at time point 3}
+#'   \item{v3/median}{Variable representing the median at time point 3}
+#'   \item{v3/sd}{Variable representing the sd at time point 3}
+#'   \item{v4/mean}{Variable representing the mean at time point 4}
+#'   \item{v4/median}{Variable representing the median at time point 4}
+#'   \item{v4/sd}{Variable representing the sd at time point 4}
+
+
+
+#'  }
+#'  
+#'
+"wide"
