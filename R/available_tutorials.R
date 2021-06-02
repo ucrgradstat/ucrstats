ac859dfc47115c5180c4a11233f6e44795b43442
unknown <christianduenas1998@gmail.com>
2021-06-02 02:20:45 -0700

    Add function to list tutorials

---
+++ R/available_tutorials.R
@@ -0,0 +1,13 @@
+#' available tutorials
+#' 
+#' A function that lists all of the available tutorials in the "ucrstats" package 
+#'
+#' 
+#'
+#' @export
+#'
+#' 
+available_tutorials <- function() {
+  return( ucrstats::tutorials)
+}
+
