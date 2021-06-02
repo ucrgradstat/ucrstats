2222d57c3a3c82410133153f85e16d12ce228923
unknown <christianduenas1998@gmail.com>
2021-06-02 02:26:44 -0700

    Add info for tutorial table

--- R/data.R
+++ R/data.R
@@ -90,5 +90,17 @@
 #'
 "lme_three"
 
+#' A table containing a list of all the tutorials in the package
+#'
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{ID}{The string placed in the `access_tutorial` function to access the tutorial}
+#'   \item{Subject}{The topic the tutorial generally relates to}
+#'   \item{Description}{A brief description of what the tutorial will cover}
+#'   
+#'  }
+#'  
+#'
 
 
